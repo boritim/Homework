b6c18d5d740c7b5317836bd14da97c57cb2ddea7
CarvenSi <borisovtim@mail.ru>
2021-06-07 00:41:26 +0300

    Homework

---
+++ Homeworkinfa/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PomoikaToConvert
+{
+    public class Startup
+    {
+        public void ConfigureServiсes(IServiceCollection service)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+            app.UseMiddleware<RoutingMiddleware>();
+        }
+    }
+}
